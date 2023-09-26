f56f0234ae9577796a10f6e66959146b2710d968
kalu661 <lukaslopez50@gmail.com>
2023-09-26 09:19:36 -0300

    rutas

--- sistema-stock/client/src/Layout/NavBar.jsx
+++ sistema-stock/client/src/Layout/NavBar.jsx
@@ -5,21 +5,25 @@ import '../pages/Login';
 
 // eslint-disable-next-line react/prop-types
 export default function NavInicio({ isLoggedIn }) {
+  // const rutaServidor = ""; //*Pruebas
+  const rutaServidor = '/sistema-stock'; //* Produccion
   return (
     <Navbar expand="lg" bg="dark" data-bs-theme="dark">
       <Container>
-        <Navbar.Brand href="/home">Sistema de Inventario</Navbar.Brand>
+        <Navbar.Brand href={rutaServidor + '/home'}>
+          Sistema de Inventario
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             {/* {isLoggedIn ? ( */}
             <>
-              <Nav.Link href="/Panel">Tabla</Nav.Link>
-              <Nav.Link href="/Form">Formulario</Nav.Link>
+              <Nav.Link href={rutaServidor + '/panel'}>Tabla</Nav.Link>
+              <Nav.Link href={rutaServidor + '/form'}>Formulario</Nav.Link>
               <Nav.Link href="/Logout">Cerrar sesión</Nav.Link>
             </>
             {/* ) : ( */}
-            <Nav.Link href="/Login">Iniciar sesión</Nav.Link>
+            <Nav.Link href={rutaServidor + '/Login'}>Iniciar sesión</Nav.Link>
             {/* )} */}
           </Nav>
         </Navbar.Collapse>
