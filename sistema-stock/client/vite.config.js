54c62c0cf62db523cbae88f1dd4a634273374aa4
kalu661 <lukaslopez50@gmail.com>
2023-09-26 09:19:30 -0300

    config

--- sistema-stock/client/vite.config.js
+++ sistema-stock/client/vite.config.js
@@ -1,25 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      name: 'sistema-stock',
-      // the proper extensions will be added
-      fileName: 'sistema-stock',
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['vue'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'Vue',
-        },
-      },
-    },
-  },
+  plugins: [react()],
+  base: '/sistema-stock/',
 });
